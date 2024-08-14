3a61c486334fd94948e957442bb0bf05a48c16b1
= <umairziyan@protonmail.com>
2024-08-14 11:54:38 +0400

    changed search functions to iterators

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,7 @@ use std::env;
 use std::process;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    // args[0] is the name of the binary
-    let config = Config::build(&args).unwrap_or_else(|err| {
+    let config = Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
